792f060d98e76d1a882f78b615381a7144a6e075
Azamat966 <reallygoooool01@gmail.com>
2023-10-11 18:05:21 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Загадай число от 1 до 20 : \n" +
+//                "после загаданного числа прибавь его к 5 , полученный результат умножь на 3 , после вычти 15 \n" +
+//                "и напиши сколько у тебя получилось \n" +
+//                "");
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        int sum = a+5;
+//        int sum1 = sum*3;
+//        int sum2 = sum1-15;
+//        int sum3 = sum2/3;
+//        System.out.println("Ты угадал "+sum3);
+//
+//
+//
+//
+//
+//
+//
+//    }
+//        Scanner scanner = new Scanner(System.in);
+//        Random random = new Random();
+//        System.out.println("Я угадал число от 1 го до 10 ти найди");
+//        int a = scanner.nextInt();
+//        int b = random.nextInt(1,5);
+//        System.out.println(b);
+//        if (b==a){
+//            System.out.println("Поздравлю ты нашол золота");
+//        }else
+//            System.out.println("Нет я загадал "+b);
+//
+//    }
+    }
+}
